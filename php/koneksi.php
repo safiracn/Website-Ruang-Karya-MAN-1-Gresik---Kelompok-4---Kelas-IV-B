d4ea97661009f7aecbda86a4e4ca62c0217ece7d
Rania Zahiyah J <24082010081@student.upnjatim.ac.id>
2026-04-10 14:38:28 +0700

    update koneksi

---
+++ php/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost:3307"; 
+$user = "root";
+$pass = "";
+$db   = "ruang_karya";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    // Kalau 3307 gagal, kita coba 3306 (standar)
+    $koneksi = mysqli_connect($host, $user, $pass, $db);
+    
+    if(!$koneksi) {
+        die("Koneksi Gagal Total: " . mysqli_connect_error());
+    }
+}
+?>
